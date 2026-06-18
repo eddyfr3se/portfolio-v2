55cc6a592dd33db6889a38c948cb24193b6ddd9d
eddyfr3se <chrisunder@gmx.de>
2026-06-18 10:59:14 +0200

    feat: add footer and legal notice page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'legal-notice', component: LegalNotice },
 ];
