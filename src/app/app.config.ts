00f42016de376bb5a0818e30955252949754fae7
eddyfr3se <chrisunder@gmx.de>
2026-08-19 16:30:12 +0200

    fix: refresh translations after deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     ),
     provideHttpClient(),
     provideTranslateService({
-      loader: provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json' }),
+      loader: provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json?v=join-project' }),
       fallbackLang: 'de',
       lang: 'de',
     }),
